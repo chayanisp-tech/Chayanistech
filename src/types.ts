478b7e5747140ee958ac92249fc892ed09c84229
chayanisp-tech <chayanisp@banmuang.ac.th>
2026-07-10 10:39:39 +0700

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -11,6 +11,7 @@ export interface Question {
   answerIndex: number; // 0-based index
   points: number;
   type?: "choice" | "subjective";
+  subjectiveInputType?: "text" | "canvas"; // 🌟 บรรทัดที่เพิ่มเข้ามาใหม่
   imageUrl?: string;
 }
 
